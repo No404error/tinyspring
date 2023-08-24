d3d7527eb323209d8782908196a21266ca7158da
34848 <3484868205@qq.com>
2023-08-24 09:05:21 +0800

    AbstractBeanDefinitionReader add comment

--- src/main/java/com/zhangkie/tinyspring/beans/reader/AbstractBeanDefinitionReader.java
+++ src/main/java/com/zhangkie/tinyspring/beans/reader/AbstractBeanDefinitionReader.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 /**
  * BeanReader能够保证每个reader都确实需要一个registry来存储读完资源后将definition放入其中，以及一个ResourceLoader
+ * 但是它不应该去实现loadBeanDefinitions,该方法要留给子类去实现
  */
 public abstract class AbstractBeanDefinitionReader implements BeanDefinitionReader{
 
