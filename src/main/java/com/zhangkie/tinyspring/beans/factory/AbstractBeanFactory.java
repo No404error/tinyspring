075866e8cdfdb7029db7d95587756055ad51d8de
34848 <3484868205@qq.com>
2023-08-24 11:32:02 +0800

    modify AbstractBeanFactory, get bean with lazy loading ,and add a func of preInstantiateSingletons

--- src/main/java/com/zhangkie/tinyspring/beans/factory/AbstractBeanFactory.java
+++ src/main/java/com/zhangkie/tinyspring/beans/factory/AbstractBeanFactory.java
@@ -2,23 +2,44 @@ package com.zhangkie.tinyspring.beans.factory;
 
 import com.zhangkie.tinyspring.beans.BeanDefinition;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public abstract class AbstractBeanFactory implements BeanFactory{
 
     private Map<String, BeanDefinition> beanDefinitionMap=new ConcurrentHashMap<>();
 
+    private Set<String> beanDefinitionNames=new HashSet<>();
+
 
     @Override
-    public Object getBean(String beanName) {
-        return beanDefinitionMap.get(beanName).getBean();
+    public Object getBean(String beanName) throws Exception {
+        BeanDefinition beanDefinition = beanDefinitionMap.get(beanName);
+        if(beanDefinition==null)
+            throw new Exception("No such bean named "+beanName);
+        Object bean = beanDefinition.getBean();
+        if(bean==null){
+            bean=doCreateBean(beanDefinition);
+            //beanDefinition.setBean(bean);
+        }
+        return bean;
     }
 
-    public void registerBeanDefinition(String name, BeanDefinition definition) throws Exception {
-        Object bean = doCreateBean(definition);
-        definition.setBean(bean);
+    public void registerBeanDefinition(String name, BeanDefinition definition){
         this.beanDefinitionMap.put(name,definition);
+        this.beanDefinitionNames.add(name);
+    }
+
+    /**
+     * 由于当前的getBean为延迟加载模式,所以需要额外添加一个能够实现提前加载的方法
+     */
+    public final void preInstantiateSingletons() throws Exception {
+        for (String beanName : beanDefinitionNames) {
+            getBean(beanName);
+        }
     }
 
     /**
