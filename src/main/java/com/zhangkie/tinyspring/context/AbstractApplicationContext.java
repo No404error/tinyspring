7848f8bc50252265abdce8f13a4afe5dd73fe23e
34848 <3484868205@qq.com>
2023-08-25 14:12:20 +0800

    AbstractApplicationContext add refresh func with support BeanPostProcessor

--- src/main/java/com/zhangkie/tinyspring/context/AbstractApplicationContext.java
+++ src/main/java/com/zhangkie/tinyspring/context/AbstractApplicationContext.java
@@ -1,6 +1,9 @@
 package com.zhangkie.tinyspring.context;
 
+import com.zhangkie.tinyspring.BeanPostProcessor;
 import com.zhangkie.tinyspring.factory.AbstractBeanFactory;
+
+import java.util.List;
 
 public abstract class AbstractApplicationContext implements ApplicationContext{
     protected AbstractBeanFactory beanFactory;
@@ -9,8 +12,33 @@ public abstract class AbstractApplicationContext implements ApplicationContext{
         this.beanFactory = beanFactory;
     }
 
-    public void refresh() throws Exception {
+    /**
+     * refresh 是为了刷新容器达到容器可用的目的
+     * 子类在被调用前一定要调用该方法
+     * 1.读取beanDefinition
+     * 2.注册BeanPostProcessor
+     * 3.自定义刷新容器
+     * @throws Exception
+     */
+    public final void refresh() throws Exception {
+        loadBeanDefinitions();
+        beanFactory.registerBeanPostProcessors();
+        onRefresh();
     }
+
+    /**
+     * 自定义刷新容器的接口
+     */
+    protected void onRefresh() throws Exception {
+    }
+
+    /**
+     * 解析所有BeanDefinition
+     * 交由子类进行实现
+     */
+    protected abstract void loadBeanDefinitions() throws Exception;
+
+
 
     @Override
     public Object getBean(String beanName) throws Exception {
