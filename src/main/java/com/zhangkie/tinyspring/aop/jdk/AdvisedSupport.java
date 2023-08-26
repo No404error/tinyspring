8557b625cb5b3a4eef56adec1286b4ccaef1bcc4
34848 <3484868205@qq.com>
2023-08-26 14:52:31 +0800

    AdvisedSupport add MethodFilter to filter function invoke

--- src/main/java/com/zhangkie/tinyspring/aop/jdk/AdvisedSupport.java
+++ src/main/java/com/zhangkie/tinyspring/aop/jdk/AdvisedSupport.java
@@ -1,16 +1,29 @@
 package com.zhangkie.tinyspring.aop.jdk;
 
+import com.zhangkie.tinyspring.aop.weaver.MethodFilter;
 import org.aopalliance.intercept.MethodInterceptor;
 
+/**
+ * 用于协助代理完成invoke调用
+ * 作用1,提供代理必备参数:target,targetClass,methodInterceptor
+ * 作用2,能够配合aspectJ expression来确定某个方法是否需要被代理(当filter为null时所有方法不代理)
+ */
 public class AdvisedSupport {
 
     private TargetSource targetSource;
 
     private MethodInterceptor methodInterceptor;
 
+    private MethodFilter methodFilter;
+
     public AdvisedSupport(TargetSource targetSource, MethodInterceptor methodInterceptor) {
+        this(targetSource,methodInterceptor,null);
+    }
+
+    public AdvisedSupport(TargetSource targetSource, MethodInterceptor methodInterceptor, MethodFilter methodFilter) {
         this.targetSource = targetSource;
         this.methodInterceptor = methodInterceptor;
+        this.methodFilter = methodFilter;
     }
 
     public TargetSource getTargetSource() {
@@ -27,5 +40,13 @@ public class AdvisedSupport {
 
     public void setMethodInterceptor(MethodInterceptor methodInterceptor) {
         this.methodInterceptor = methodInterceptor;
+    }
+
+    public MethodFilter getMethodFilter() {
+        return methodFilter;
+    }
+
+    public void setMethodFilter(MethodFilter methodFilter) {
+        this.methodFilter = methodFilter;
     }
 }
