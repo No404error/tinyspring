f38d8e5833445646f92577fe6b846163aa912ea1
34848 <3484868205@qq.com>
2023-08-26 20:14:13 +0800

    TargetSource modify some paramter and remove member of advisedSupport from AbstractAopProxy

--- src/main/java/com/zhangkie/tinyspring/aop/jdk/JDKAopProxy.java
+++ src/main/java/com/zhangkie/tinyspring/aop/jdk/JDKAopProxy.java
@@ -1,6 +1,8 @@
 package com.zhangkie.tinyspring.aop.jdk;
 
-import com.zhangkie.tinyspring.aop.AopProxy;
+import com.zhangkie.tinyspring.aop.AdvisedSupport;
+import com.zhangkie.tinyspring.aop.AbstractAopProxy;
+import com.zhangkie.tinyspring.aop.ReflectiveMethodInvocation;
 import com.zhangkie.tinyspring.aop.weaver.MethodFilter;
 import org.aopalliance.intercept.MethodInterceptor;
 
@@ -8,17 +10,15 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
-public class JDKAopProxy implements AopProxy, InvocationHandler {
-
-    private AdvisedSupport advisedSupport;
+public class JDKAopProxy extends AbstractAopProxy implements InvocationHandler {
 
     public JDKAopProxy(AdvisedSupport advisedSupport) {
-        this.advisedSupport = advisedSupport;
+        super(advisedSupport);
     }
 
     @Override
     public Object getProxy() {
-        return Proxy.newProxyInstance(this.getClass().getClassLoader(), advisedSupport.getTargetSource().getTargetClasses(),this);
+        return Proxy.newProxyInstance(this.getClass().getClassLoader(), advisedSupport.getTargetSource().getInterfaces(),this);
     }
 
     @Override
@@ -26,8 +26,8 @@ public class JDKAopProxy implements AopProxy, InvocationHandler {
         MethodInterceptor methodInterceptor = advisedSupport.getMethodInterceptor();
         MethodFilter methodFilter = advisedSupport.getMethodFilter();
         //代理的方法在expression范围内
-        if(methodFilter!=null&&methodFilter.match(advisedSupport.getTargetSource().getTarget().getClass(),method))
-            return methodInterceptor.invoke(new ReflectiveMethodInterceptor(advisedSupport.getTargetSource().getTarget(),method,args));
+        if(methodFilter==null||methodFilter.match(advisedSupport.getTargetSource().getTarget().getClass(),method))
+            return methodInterceptor.invoke(new ReflectiveMethodInvocation(advisedSupport.getTargetSource().getTarget(),method,args));
         else{
             return method.invoke(advisedSupport.getTargetSource().getTarget(),args);
         }
